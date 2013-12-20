a32162b95acbc84cc09553a1bd526c3275511744
szensk <eleinvisible@gmail.com>
2013-12-19 21:30:24 -0600

    completions work a bit better

--- completions.py
+++ completions.py
@@ -7,7 +7,7 @@ class LoveCompletions(sublime_plugin.EventListener):
     ST = 3000 if sublime.version() == '' else int(sublime.version())
 
     def on_query_completions(self, view, prefix, locations):
-        if self.ST < 3000:
+        if self.ST < 3000 and ("lua" in view.scope_name(locations[0])):
             seps = view.settings().get("word_separators")
             seps = seps.replace('.', '')
             view.settings().set("word_separators", seps)
